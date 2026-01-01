8bdd9d61af204eb479073fed69d7f718995ca26c
Frank Lee <franklee@huelight.kr>
2026-01-01 23:39:35 +0900

    v1

--- components/sections/DualPromo.tsx
+++ components/sections/DualPromo.tsx
@@ -4,7 +4,7 @@ import { Container } from '@/components/ui/Container';
 
 export function DualPromo() {
     return (
-        <section className="py-16 lg:py-24 bg-white">
+        <section id="dual-promo" className="relative z-10 py-16 lg:py-24 bg-white">
             <Container>
                 <div className="grid md:grid-cols-2 gap-6 lg:gap-8">
 
